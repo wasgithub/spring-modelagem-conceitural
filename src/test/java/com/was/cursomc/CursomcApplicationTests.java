a4965a89f4fd2dc5a21cfaa7638af71029bda090
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2019-11-08 13:15:21 -0300

    Commit Inicial: Projeto Criado

---
+++ src/test/java/com/was/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.was.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
