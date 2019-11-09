c938437217db7a1d3d9ca5c66c3459538296d033
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2019-11-08 22:32:18 -0300

    produto e associação muito para muitos

--- src/main/java/com/was/cursomc/CursomcApplication.java
+++ src/main/java/com/was/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.was.cursomc.domain.Categoria;
+import com.was.cursomc.domain.Produto;
 import com.was.cursomc.repositories.CategoriaRepository;
+import com.was.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 
 	public static void main(String[] args) {
@@ -26,7 +31,16 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto prod1 =  new Produto(null, "Computador", 1000.00);
+		Produto prod2 =  new Produto(null, "Impressora", 500.00);
+		Produto prod3 =  new Produto(null, "Mouse", 50.00);
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }
