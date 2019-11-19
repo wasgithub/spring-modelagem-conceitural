cb90046809d1e0989ed96d5bf0b3c2bccc30d4f7
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2019-11-19 12:56:57 -0300

    Inserindo uma categoria com Post

--- src/main/java/com/was/cursomc/services/CategoriaService.java
+++ src/main/java/com/was/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.was.cursomc.domain.Categoria;
 import com.was.cursomc.repositories.CategoriaRepository;
@@ -19,6 +22,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! " + id + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
