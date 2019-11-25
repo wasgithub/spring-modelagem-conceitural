1c10db2c53720e4d2a787ec9bbdfc473ae169b97
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2019-11-25 08:00:42 -0300

    CATEGORIA(DELETE): Inplementando método delete, adicionando exceção

--- src/main/java/com/was/cursomc/services/CategoriaService.java
+++ src/main/java/com/was/cursomc/services/CategoriaService.java
@@ -3,13 +3,12 @@ package com.was.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.was.cursomc.domain.Categoria;
 import com.was.cursomc.repositories.CategoriaRepository;
+import com.was.cursomc.services.exceptions.DataIntegrityException;
 import com.was.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 
 }
